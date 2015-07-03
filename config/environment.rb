67e92d2a880535b4731faa09acd0690cc2c9baa2
Randolph Kok <cherwa.kok@gmail.com>
2015-07-03 01:14:14 +0000

    Fixed environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ ActionMailer::Base.smtp_settings = {
   :port => '587',
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SNEDGRID_PASSWORD'], 
+  :password => ENV['SENDGRID_PASSWORD'], 
   :domain => 'heroku.com',
   :enable_startstls_auto => ture
   }

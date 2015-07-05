fb6ce20c3ce2d7ba94d8691789115767e827fd29
Randolph Kok <cherwa.kok@gmail.com>
2015-07-05 01:40:26 +0000

    Fixed typos in contoller file and routes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,10 @@ class Users::RegistartionsController < Devise::RegistrationsController
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
-        if resouce.plan_id == 2
-          resouce.save_with_payment
+        if resource.plan_id == 2
+          resource.save_with_payment
         else
-          resouce.save
+          resource.save
         end
       end
     end

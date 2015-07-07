4c248be740589f5f97bd689ea069580b1fa026d1
Randolph Kok <cherwa.kok@gmail.com>
2015-07-07 10:27:05 +0000

    Added profiles form and create action

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+#   validates :first_name, presence: true
+#   vaildates :last_name, presence: true
+#   vaildates :contact_email, presence: true
 end
